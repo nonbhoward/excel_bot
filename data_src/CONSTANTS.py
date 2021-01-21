ec06d6b64f058cc71961311a9ff91de4237c2437
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-01-21 16:28:18 -0500

    add CONSTANTS

---
+++ data_src/CONSTANTS.py
@@ -0,0 +1,7 @@
+from minimalog.minimal_log import MinimalLog
+ml = MinimalLog(__name__)
+event = 'importing {}'.format(__name__)
+print(event)
+ml.log_event(event)
+
+EXCEL_EXTS = ['xlsm', 'xlsx', 'xltm', 'xltx']
